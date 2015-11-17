e2a6b31ca69d23afd76744356e1fd920d0536dd3
Anders Selborn (ueh093) <anders.selborn@teliasonera.com>
2015-11-17 13:36:53 +0100

    IGLOO-0000: txtboxes

--- telldus.js
+++ telldus.js
@@ -5,7 +5,14 @@ $(document).ready(function(){
 	var rowCount = 1;
 	$(function(){
 		$("#addRow").click(function () {
-			$('#topTable').append('<tr><td'id='rowCount' + rowCount><input type='textbox'/input> </td></tr>');
+			var createRowText = "<tr align=center>"
+			createRowText += '<td><input type="textbox" id="deviceID_" +rowCount value="" name="deviceId" /></td>';
+			createRowText += '<td><input type="textbox" id="deviceName" +rowCount value="" name="deviceName" /></td>';
+
+
+			createRowText +="</tr>";
+
+			$('#topTable').append(createRowText);
 			rowCount++;
 		});
 	});
